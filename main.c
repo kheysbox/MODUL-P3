38a5ddbf8682e9570e67d74a5ef9df65fa7021da
kheysbox <kheysbox@gmail.com>
2025-09-26 22:30:53 +0700

    TUGAS ASISTENSI 3

--- main.c
+++ main.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+
 void desimal_to_biner(int n) {
     int i = 0;
     int asli = n;
     int biner[32];
 
-
-    while (n > 0) {     // konversi ke biner
+    while (n > 0) {
         biner[i] = n % 2;
         n = n / 2;
-        i++; }
+        i++;
+    }
+    
     printf("Biner dari %d : ", asli);
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", biner[j]);
-
     }
     
     printf("\n");
-    printf("4-bit: ");     // format 4 bit
+    printf("Format 4-bit: ");
+    
     int count = 0;
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", biner[j]);
         count++;
         if (count % 4 == 0 && j != 0) {
-            printf(" ");}}
+            printf(" ");
+        }
+    }
+    
     printf("\n");
-
-    if (asli % 2 == 0)     // cek genap atau ganjil=
+    
+    if (asli % 2 == 0)
         printf("Genap\n");
     else
         printf("Ganjil\n");
@@ -35,7 +40,7 @@ int main() {
     int angka;
     printf("Input nilai desimal: ");
     scanf("%d", &angka);
-
+    
     desimal_to_biner(angka);
     return 0;
 }
